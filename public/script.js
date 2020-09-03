95ec0d15133a164f49d850edb7308f4614169b43
mark <markpackham1@gmail.com>
2020-09-03 14:49:41 +0100

    show room joined by user

--- public/script.js
+++ public/script.js
@@ -1,2 +1,8 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
+
+socket.emit("join-room", ROOM_ID, 10);
+
+socket.on("user-connected", (userId) => {
+  console.log("User connected: " + userId);
+});
